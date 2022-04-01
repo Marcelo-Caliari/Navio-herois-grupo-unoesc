e2ca77dc2c41e490d22d0086a8080e8c74b6ad69
Felipe Matheus da Rosa <400755@unoesc.lan>
2022-03-31 21:31:58 -0300

    naviosherois

--- naviosherois.py
+++ naviosherois.py
@@ -1,2 +1,7 @@
 import random
 print (random.randrange(0,11) )
+
+resposta_permitido_atacar = input("deseja atacar?s/n")
+is_permitido_atacar=False 
+  if(resposta_permitido_atacar == "s"): 
+    is_permitido_atacar = True
