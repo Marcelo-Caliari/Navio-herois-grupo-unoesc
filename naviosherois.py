5d5dd591f03c5dcfe87ea9903831c2086a3edb47
Felipe Matheus da Rosa <400755@unoesc.lan>
2022-03-31 21:36:59 -0300

    atualização condição atacar vilões

--- naviosherois.py
+++ naviosherois.py
@@ -3,5 +3,8 @@ print (random.randrange(0,11) )
 
 resposta_permitido_atacar = input("deseja atacar?s/n")
 is_permitido_atacar=False 
-  if(resposta_permitido_atacar == "s"): 
+if(resposta_permitido_atacar == "s"): 
     is_permitido_atacar = True
+    print("os heróis vão atacar")
+if(resposta_permitido_atacar == "n"):
+    print("os vilões vão atacar o navio")
